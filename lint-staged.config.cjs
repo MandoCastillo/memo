f040501c5ce0875a17fdd5ef406f4f1862aff370
Armando Castillo <armandocaslop@gmail.com>
2022-12-16 12:05:53 -0600

    chore(eslint-prettier):  config eslint and prettier rules

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
   // This will lint and format TypeScript and JavaScript files
-  // '**/*.(ts|tsx|js|jsx)': (filenames) => [
-  //   `yarn eslint --fix ${filenames.join(' ')}`,
-  //   `yarn prettier --write ${filenames.join(' ')}`,
-  // ],
+  '**/*.(ts|tsx|js|jsx)': filenames => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`
+  ]
 };
