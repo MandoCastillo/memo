2090bb92004b51f65708ba013545d220febf71bb
Armando Castillo <armandocaslop@gmail.com>
2022-12-17 14:06:31 -0600

    feat(boards):  separete component logic in custom hook

--- src/modules/memorama/components/board/board.component.tsx
+++ src/modules/memorama/components/board/board.component.tsx
@@ -1,116 +1,31 @@
-import { useState } from 'react';
-
 import { Button, SimpleGrid, Stack, Text } from '@chakra-ui/react';
-
-import { arrayHelper, uuidHelper } from '@/helpers';
-import { ICard } from '@/interfaces';
+import ReactConfetti from 'react-confetti';
 
 import { Card } from '../card';
 
-const cardsInitial: ICard[] = Array.from(Array(9).keys())
-  .concat(Array.from(Array(9).keys()))
-  .map(num => ({
-    id: uuidHelper.generateUUID(),
-    image: `/cards/00${num + 1}.png`,
-    isSelected: false
-  }));
+import { useBoard } from './use-boards';
 
 export const Board = () => {
-  const [cards, setCards] = useState<ICard[]>(arrayHelper.shuffle(cardsInitial));
-  const [selectedCards, setSelectedCards] = useState<ICard[]>([]);
-  const [areDifferent, setAreDifferent] = useState(false);
-  const [attempts, setAttempts] = useState<number>(0);
-
-  const areAllCardsSelected = cards.every(card => card.isSelected);
-
-  const handleResetGame = () => {
-    setCards(arrayHelper.shuffle(cardsInitial));
-    setSelectedCards([]);
-    setAreDifferent(false);
-    setAttempts(0);
-  };
-
-  const handleCardClick = (cardSelected: ICard) => {
-    if (areDifferent) {
-      return;
-    }
-
-    // if selectedCards has 1 card, check if it's the same card
-    if (selectedCards.length === 1 && selectedCards[0].id === cardSelected.id) {
-      return;
-    }
-
-    // add card to selectedCards and change state
-    if (selectedCards.length === 0) {
-      setSelectedCards([cardSelected]);
-
-      const cardIdx = cards.findIndex(card => card.id === cardSelected.id);
-      const cardsCopy = [...cards];
-      cardsCopy[cardIdx] = {
-        ...cardsCopy[cardIdx],
-        isSelected: true
-      };
-
-      setCards(cardsCopy);
-      return;
-    }
-
-    // add new attempt
-    setAttempts(attempts + 1);
-
-    // cards are equal
-    if (selectedCards.length === 1 && selectedCards[0].image === cardSelected.image) {
-      const cardIdx = cards.findIndex(card => card.id === cardSelected.id);
-      const cardsCopy = [...cards];
-      cardsCopy[cardIdx] = {
-        ...cardsCopy[cardIdx],
-        isSelected: true
-      };
-
-      setCards(cardsCopy);
-      setSelectedCards([]);
-    }
-
-    // cards are different
-    if (selectedCards.length === 1 && selectedCards[0].image !== cardSelected.image) {
-      const cardIdx = cards.findIndex(card => card.id === cardSelected.id);
-      const cardsCopy = [...cards];
-      cardsCopy[cardIdx] = {
-        ...cardsCopy[cardIdx],
-        isSelected: true
-      };
-
-      setCards(cardsCopy);
-      setSelectedCards([]);
-      setAreDifferent(true);
-
-      setTimeout(() => {
-        const newCardsRevert = cards.map(card => {
-          if (card.id === cardSelected.id || card.id === selectedCards[0].id) {
-            return {
-              ...card,
-              isSelected: false
-            };
-          }
-          return card;
-        });
-        setCards(newCardsRevert);
-        setAreDifferent(false);
-      }, 1000);
-    }
-  };
+  const { attempts, areAllCardsSelected, cards, handleCardClick, handleResetGame } = useBoard();
 
   return (
-    <Stack gap={4} p={2}>
-      <Text textAlign="center">Attempts: {attempts}</Text>
-      <SimpleGrid columns={24} gap={4}>
-        {cards.map(card => (
-          <Card key={card.id} card={card} handleCardClick={handleCardClick} />
-        ))}
-      </SimpleGrid>
-      <Button onClick={handleResetGame}>
-        {areAllCardsSelected ? 'You win! Resert' : 'Reset game'}
-      </Button>
-    </Stack>
+    <>
+      {areAllCardsSelected && (
+        <ReactConfetti width={window.innerWidth} height={window.innerHeight} />
+      )}
+      <Stack gap={4} p={2}>
+        <Text textAlign="center" fontSize="3xl">
+          Attempts: {attempts}
+        </Text>
+        <SimpleGrid columns={24} gap={4}>
+          {cards.map(card => (
+            <Card key={card.id} card={card} handleCardClick={handleCardClick} />
+          ))}
+        </SimpleGrid>
+        <Button onClick={handleResetGame} colorScheme={areAllCardsSelected ? 'teal' : 'gray'}>
+          {areAllCardsSelected ? 'You win! Reset' : 'Reset game'}
+        </Button>
+      </Stack>
+    </>
   );
 };
---
+++ src/modules/memorama/components/board/use-boards.ts
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+
+import { arrayHelper, uuidHelper } from '@/helpers';
+import { ICard } from '@/interfaces';
+
+const cardsInitial: ICard[] = Array.from(Array(9).keys())
+  .concat(Array.from(Array(9).keys()))
+  .map(num => ({
+    id: uuidHelper.generateUUID(),
+    image: `/cards/00${num + 1}.png`,
+    isSelected: false
+  }));
+
+export const useBoard = () => {
+  const [cards, setCards] = useState<ICard[]>(arrayHelper.shuffle(cardsInitial));
+  const [selectedCards, setSelectedCards] = useState<ICard[]>([]);
+  const [areDifferent, setAreDifferent] = useState(false);
+  const [attempts, setAttempts] = useState<number>(0);
+
+  const areAllCardsSelected = cards.every(card => card.isSelected);
+
+  const handleResetGame = () => {
+    setCards(arrayHelper.shuffle(cardsInitial));
+    setSelectedCards([]);
+    setAreDifferent(false);
+    setAttempts(0);
+  };
+
+  const handleCardClick = (cardSelected: ICard) => {
+    if (areDifferent) {
+      return;
+    }
+
+    // if selectedCards has 1 card, check if it's the same card
+    if (selectedCards.length === 1 && selectedCards[0].id === cardSelected.id) {
+      return;
+    }
+
+    // add card to selectedCards and change state
+    if (selectedCards.length === 0) {
+      setSelectedCards([cardSelected]);
+
+      const cardIdx = cards.findIndex(card => card.id === cardSelected.id);
+      const cardsCopy = [...cards];
+      cardsCopy[cardIdx] = {
+        ...cardsCopy[cardIdx],
+        isSelected: true
+      };
+
+      setCards(cardsCopy);
+      return;
+    }
+
+    // add new attempt
+    setAttempts(attempts + 1);
+
+    // cards are equal
+    if (selectedCards.length === 1 && selectedCards[0].image === cardSelected.image) {
+      const cardIdx = cards.findIndex(card => card.id === cardSelected.id);
+      const cardsCopy = [...cards];
+      cardsCopy[cardIdx] = {
+        ...cardsCopy[cardIdx],
+        isSelected: true
+      };
+
+      setCards(cardsCopy);
+      setSelectedCards([]);
+    }
+
+    // cards are different
+    if (selectedCards.length === 1 && selectedCards[0].image !== cardSelected.image) {
+      const cardIdx = cards.findIndex(card => card.id === cardSelected.id);
+      const cardsCopy = [...cards];
+      cardsCopy[cardIdx] = {
+        ...cardsCopy[cardIdx],
+        isSelected: true
+      };
+
+      setCards(cardsCopy);
+      setSelectedCards([]);
+      setAreDifferent(true);
+
+      setTimeout(() => {
+        const newCardsRevert = cards.map(card => {
+          if (card.id === cardSelected.id || card.id === selectedCards[0].id) {
+            return {
+              ...card,
+              isSelected: false
+            };
+          }
+          return card;
+        });
+        setCards(newCardsRevert);
+        setAreDifferent(false);
+      }, 700);
+    }
+  };
+
+  return {
+    attempts,
+    cards,
+    areAllCardsSelected,
+    handleCardClick,
+    handleResetGame
+  };
+};
